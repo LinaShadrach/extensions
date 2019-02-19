6ecaaea8b0e90599590716d741cdcee500247614
Lina Shadrach <15620627+LinaShadrach@users.noreply.github.com>
2019-02-19 13:59:05 -0800

    change switch interval to use variable

--- pair-chrome-extension/background.js
+++ pair-chrome-extension/background.js
@@ -1,6 +1,8 @@
+var switchInterval = 21000;
+
 var startNotifications = function() {
   var now = new Date();
-  var notificationInterval = setInterval(createNotification, 1000);
+  var notificationInterval = setInterval(createNotification, switchInterval);
   var classTime = (now.getHours() >= 8 && now.getHours() <= 16 && now.getDay() !== 0 && now.getDay() !== 5 && now.getDay() !== 6);
   if(!classTime) { clearInterval(notificationInterval) }
 }
@@ -9,11 +11,11 @@ var opt = {
     title: 'Switch Pairs!',
     message: 'time\'s up',
     priority: 1,
-    expandedMessage: 'hi',
+    expandedMessage: `It's been ${switchInterval/100} minutes since the last switch. if you haven't switched already, please switch driver/navigator roles.`,
     requireInteraction: true,
     iconUrl:'epicodus-icon.png'
-};
+}
 var createNotification = function() {
-  chrome.notifications.create('pair-notification', opt, function(){console.log("print")})
+  chrome.notifications.create('pair-notification', opt, null)
 }
 chrome.runtime.onInstalled.addListener(startNotifications);
